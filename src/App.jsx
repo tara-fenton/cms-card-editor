56f738d213599bbf35645bce1f87dde503a4e446
Tara Fenton <tarafenton21@gmail.com>
2025-12-18 14:30:32 -0500

    scaffold vite app, cleanup  and folder structure

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        welcome
+      </div>
+    </>
+  )
+}
+
+export default App
